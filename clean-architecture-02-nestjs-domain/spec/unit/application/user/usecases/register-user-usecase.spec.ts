52a457b73ad114cdd8281fbe339f4d9138b92d63
Donny Roufs <iamchets@yahoo.com>
2021-08-03 08:43:23 +0200

    add todos

--- clean-architecture-02-nestjs-domain/spec/unit/application/user/usecases/register-user-usecase.spec.ts
+++ clean-architecture-02-nestjs-domain/spec/unit/application/user/usecases/register-user-usecase.spec.ts
@@ -1,3 +1,6 @@
 describe('register-user-usecase', () => {
-  test.todo('Should work');
+  test.todo('it should create a new domain entity and pass a unique id');
+  test.todo('it should save the entity');
+  test.todo('it should return an error when failed to save');
+  test.todo('it should return a mapped entity');
 });
